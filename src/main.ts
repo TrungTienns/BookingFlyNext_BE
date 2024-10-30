4b7ef65c2d6a8b9d1cc551c1e9a4194afc1c6389
TrungTien <22dh113737@st.huflit.edu.vn>
2024-10-30 08:48:20 +0700

    Setup Prisma

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as express from 'express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { IoAdapter } from '@nestjs/platform-socket.io';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors();
+  app.use(express.static('.'));
+
+  const config = new DocumentBuilder()
+    .setTitle('FlyNext Swagger')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/swagger', app, document);
+
+  await app.listen(8080);
 }
 bootstrap();
